ece92eef31e39087cf33af5d58a6a6fa5e159934
Alejandro Ortega <161636827+AlexOrtega301@users.noreply.github.com>
2026-03-11 17:52:35 -0600

    Create Time Module.py

---
+++ Python/Time Module.py
@@ -0,0 +1,14 @@
+import datetime  
+    
+# using now() to get current time 
+current_time = datetime.datetime.now()  
+    
+# Printing value of now.  
+print("Time now at greenwich meridian is : ", end = "")   
+print(current_time)
+
+# print calendar of year 2025
+import calendar
+print("\n", calendar.calendar(1969))
+
+## Ok that is preetty sick! Not in UnIX Time though :/
